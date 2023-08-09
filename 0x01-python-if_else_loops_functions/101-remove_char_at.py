dc12e50044535ad9f09b0d05f0f1cedc308a6af3
blessy-essy <esthermbira000@gmail.com>
2023-08-09 23:41:07 +0000

    Corrections

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,7 +3,8 @@
 
 
 def remove_char_at(str, n):
-    """Create a copy of the string while removing the character at position n."""
+    """Create a copy of the string while removing the character at position n.
+"""
     if n < 0:
         return (str)
     return (str[:n] + str[n+1:])
